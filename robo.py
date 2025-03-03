4c1c562a95842a7106b2043d59cb84a030a7f47f
Hemant Mohanty <116831187+hemantmohanty@users.noreply.github.com>
2025-03-03 05:23:19 -0800

    Update robo.py( Just add while loop)

--- robo.py
+++ robo.py
@@ -2,7 +2,10 @@ import pyttsx3
 
 if __name__ == '__main__':
     print("Welcome to roboSpeaker 1.1 Created by Hemant!")
-    x = input("Enter what you want me to speak: ")
-    engine = pyttsx3.init()
-    engine.say(x)
-    engine.runAndWait()
+    while True:
+        x = input("Enter what you want me to speak: ")
+        if x == 'q':
+            break
+        engine = pyttsx3.init()
+        engine.say(x)
+        engine.runAndWait()
